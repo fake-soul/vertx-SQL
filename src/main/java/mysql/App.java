5e054ea1876dbb159f81c564279c4432e785fc70
Bharat Mulchandani <bharat.mulchandani@C17SMSP8GVC1.local>
2019-09-11 10:21:54 +0530

    Req body

--- src/main/java/mysql/App.java
+++ src/main/java/mysql/App.java
@@ -1,7 +1,13 @@
 package mysql;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.sql.SQLClient;
+import io.vertx.ext.web.Route;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.BodyHandler;
 import io.vertx.mysqlclient.MySQLClient;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
@@ -10,6 +16,7 @@ import io.vertx.sqlclient.PoolOptions;
 import io.vertx.sqlclient.Row;
 import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.SqlConnection;
+import sun.security.provider.certpath.Vertex;
 //import io.vertx.ext.asyncsql;
 
 /**
@@ -21,71 +28,45 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        MySQLConnectOptions connectOptions = new MySQLConnectOptions()
-                .setPort(3306)
-                .setHost("localhost")
-                .setDatabase("the-db")
-                .setUser("root")
-                .setPassword("pass@123");
-//
-        // Pool options
-        PoolOptions poolOptions = new PoolOptions()
-                .setMaxSize(5);
-//
-        // Create the client pool
-        MySQLPool client = MySQLPool.pool(connectOptions, poolOptions);
+//        MySQLConnectOptions connectOptions = new MySQLConnectOptions()
+//                .setPort(3306)
+//                .setHost("localhost")
+//                .setDatabase("the-db")
+//                .setUser("root")
+//                .setPassword("pass@123");
+////
+//        // Pool options
+//        PoolOptions poolOptions = new PoolOptions()
+//                .setMaxSize(5);
+////
+//        // Create the client pool
+//        MySQLPool client = MySQLPool.pool(connectOptions, poolOptions);
 
-        // A simple query
-        client.query("SELECT * FROM users WHERE fname='raj'", ar -> {
-            if (ar.succeeded()) {
-                RowSet result = ar.result();
-                System.out.println("Got " + result.size() + " rows ");
-                System.out.println("data ");
+        Vertx vertx = Vertx.vertx();
+        HttpServer httpServer = vertx.createHttpServer();
 
-//                ar.result()
-                for (Row row : result) {
-                    System.out.println("User " + ((Integer) ((MySQLRowImpl) row).get(0)).intValue());
-                    System.out.println("User " + ((String) ((MySQLRowImpl) row).get(1)).toString());
-                    // ((Integer) ((MySQLRowImpl) row).get(0)).intValue()
-                }
+        Router router = Router.router(vertx);
+
+        // This body handler will be called for all routes
+        router.route().handler(BodyHandler.create());
+
+        Route hamdler1 = router
+                .route("/hello")
+                .handler(routingContext -> {
+                    HttpServerResponse httpServerResponse = routingContext.response();
+                    httpServerResponse.end("yes temp");
+                    JsonObject bodyHandler = routingContext.getBodyAsJson();
+                    System.out.println(routingContext.getBody());
+
+                });
 
 
-            } else {
-                System.out.println("Failure: " + ar.cause().getMessage());
-            }
 
-            // Now close the pool
-            client.close();
-        });
 
-//        // Get a connection from the pool
-//        client.getConnection(ar1 -> {
-//
-//            if (ar1.succeeded()) {
-//
-//                System.out.println("Connected");
-//
-//                // Obtain our connection
-//                SqlConnection conn = ar1.result();
-//
-//                // All operations execute on the same connection
-//                conn.query("SELECT * FROM users WHERE id='julien'", ar2 -> {
-//                    if (ar2.succeeded()) {
-//                        conn.query("SELECT * FROM users WHERE id='emad'", ar3 -> {
-//                            // Release the connection to the pool
-//                            System.out.println("Yesy");
-//                            System.out.println(ar3);
-//                            conn.close();
-//                        });
-//                    } else {
-//                        // Release the connection to the pool
-//                        conn.close();
-//                    }
-//                });
-//            } else {
-//                System.out.println("Could not connect: " + ar1.cause().getMessage());
-//            }
-//        });
+        httpServer
+                .requestHandler(router::accept)
+                .listen(3000);
+
 
     }
 }
